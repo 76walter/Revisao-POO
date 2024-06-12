70a1acd1015e7e88df63a25969113ca9c10ddbba
76walter <145629073+76walter@users.noreply.github.com>
2024-06-12 12:27:35 -0300

    Add files via upload

---
+++ revisao2av_questao8/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.util.Date;
+
+public class App 
+{
+    public static void main( String[] args )
+    {   
+        Date data = new Date(1980);
+        Livro novoLivro = new Livro("Homens e Caranguejos", "Josue de Castro", data);
+
+    }
+}
